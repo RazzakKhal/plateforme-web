65afd79e7348c12d514250f60f3bf370f0c83fbc
razzak <khalfallah.razzak@gmail.com>
2025-01-15 22:48:09 +0100

    feat(ngrx): add of ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { Store } from '@ngrx/store';
+import { addUser } from './store/users/actions/users.actions';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [RouterOutlet, MatButtonModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  state! : Observable<any>;
+
+  constructor(private store: Store){}
+
+  ngOnInit(): void {
+   this.store.select((state) => state).subscribe((state) => {console.log('state', state)})
+  }
+
+  testDispatch(){
+    this.store.dispatch(addUser({user: 'razzak'}))
+  }
+
   title = 'web';
 }
