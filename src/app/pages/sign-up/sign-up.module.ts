e0142750ae249ef489df3f4a843c840e5c5b7b7f
razzak <khalfallah.razzak@gmail.com>
2025-01-28 01:40:52 +0100

    feat(inscription): make sign up page, need to implement toggle and make form working

--- src/app/pages/sign-up/sign-up.module.ts
+++ src/app/pages/sign-up/sign-up.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from "@angular/core";
 import { SignUpComponent } from "./sign-up/sign-up.component";
 import { RouterModule } from "@angular/router";
 import { ROUTES } from "./sign-up.routes";
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
     declarations:[SignUpComponent],
-    imports:[RouterModule.forChild(ROUTES)],
+    imports:[RouterModule.forChild(ROUTES), MatFormFieldModule, MatInputModule, MatIconModule],
     providers:[],
     exports:[],
 })
