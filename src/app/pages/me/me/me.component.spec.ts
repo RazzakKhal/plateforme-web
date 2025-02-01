13775800fe400442de0bec65b8573320a8d9fcfd
razzak <khalfallah.razzak@gmail.com>
2025-02-01 22:41:49 +0100

    chore: fix unit tests

--- src/app/pages/me/me/me.component.spec.ts
+++ src/app/pages/me/me/me.component.spec.ts
@@ -1,17 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { MeComponent } from './me.component';
+import { Store } from '@ngrx/store';
+import { MeState } from '../../../store/users/reducer/users.reducer';
 
 describe('MeComponent', () => {
   let component: MeComponent;
   let fixture: ComponentFixture<MeComponent>;
+  let store: MockStore<{ meState: MeState }>;
+
+  const initialState = {
+    meState: {
+      user: { id: 1, name: 'John Doe', email: 'john@example.com' },
+    }
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MeComponent]
+      declarations: [MeComponent],
+      imports: [],
+      providers: [        
+        provideMockStore({ initialState }) 
+      ]
     })
     .compileComponents();
 
+    store = TestBed.inject(Store) as MockStore<{ meState: MeState }>;
     fixture = TestBed.createComponent(MeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
