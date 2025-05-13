d9bb2058a57f03788e163607bac5a6efd3278565
razzak <khalfallah.razzak@gmail.com>
2025-05-14 01:37:14 +0200

    ok

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
-    production: true,
-    userBaseUri: 'https://mon-api.com'
+      production: true,
+      userBaseUri: 'http://168.231.86.167:8072', // IP publique ou nom de domaine du backend
+      userService: 'user-service',
+      formulaService: 'formula-service',
+      paymentService: 'payment-service'
 }
