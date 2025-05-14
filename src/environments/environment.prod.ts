c600756a78206e580eb9b44ac7a09a20120d0152
razzak <khalfallah.razzak@gmail.com>
2025-05-14 19:42:59 +0200

    ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
       production: true,
-      userBaseUri: 'https://book-n-drive.fr', // IP publique ou nom de domaine du backend
+      userBaseUri: 'https://book-n-drive.fr/api', // IP publique ou nom de domaine du backend
       userService: 'user-service',
       formulaService: 'formula-service',
       paymentService: 'payment-service'
