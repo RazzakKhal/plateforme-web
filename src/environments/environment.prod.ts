23bf1916b25ad5bc408ceab2e0db76170ef509ac
razzak <khalfallah.razzak@gmail.com>
2025-05-14 19:20:56 +0200

    updating of prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
       production: true,
-      userBaseUri: 'http://168.231.86.167:8072', // IP publique ou nom de domaine du backend
+      userBaseUri: 'https://book-n-drive.fr', // IP publique ou nom de domaine du backend
       userService: 'user-service',
       formulaService: 'formula-service',
       paymentService: 'payment-service'
