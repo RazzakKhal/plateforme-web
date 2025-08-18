070c542f768e3a2080c236c80953df2e60e77f1e
razzak <khalfallah.razzak@gmail.com>
2025-08-18 20:21:43 +0200

    feature/payment-return

---
+++ src/app/pages/payment/failed-payment/failed-payment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-failed-payment',
+  standalone: false,
+  templateUrl: './failed-payment.component.html',
+  styleUrl: './failed-payment.component.css'
+})
+export class FailedPaymentComponent {
+
+}
