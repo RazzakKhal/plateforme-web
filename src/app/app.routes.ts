787acb9126937614c9df9a1cdd2d21fb25d470b8
razzak <khalfallah.razzak@gmail.com>
2026-06-23 23:19:59 +0200

    ask-32: ajout des pages errors succes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,38 +2,55 @@ import { Routes } from '@angular/router';
 import { authGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'student',
-        pathMatch: 'full'
-      },
-    {
-        path:'admin', loadChildren : () => import('./pages/admin/admin.module').then(m => m.AdminModule)
-    },
-    {   
-        path:'moniteur', loadChildren: () => import('./pages/monitor/monitor.module').then(m => m.MonitorModule)
-    },
-    {
-        path:'sign-in', loadChildren: () => import('./pages/sign-in/sign-in.module').then(m => m.SignInModule)
-    },
-    {
-        path:'sign-up', loadChildren: () => import('./pages/sign-up/sign-up.module').then(m => m.SignUpModule)
-    },
-    {
-        path:'forgot-password', loadChildren: () => import('./pages/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule)
-    },
-    {
-        path:'reset-password', loadChildren: () => import('./pages/reset-password/reset-password.module').then(m => m.ResetPasswordModule)
-    },
-    {
-        path:'student',    
-        canActivate: [authGuard],
-        loadChildren: () => import('./pages/student/student.module').then(m => m.StudentModule)
-    },
-    {
-        path:'payment',    
-        // canActivate: [authGuard],
-        loadChildren: () => import('./pages/payment/payment.module').then(m => m.PaymentModule)
-    },
-    
+  {
+    path: '',
+    redirectTo: 'student',
+    pathMatch: 'full',
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./pages/admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: 'moniteur',
+    loadChildren: () =>
+      import('./pages/monitor/monitor.module').then((m) => m.MonitorModule),
+  },
+  {
+    path: 'sign-in',
+    loadChildren: () =>
+      import('./pages/sign-in/sign-in.module').then((m) => m.SignInModule),
+  },
+  {
+    path: 'sign-up',
+    loadChildren: () =>
+      import('./pages/sign-up/sign-up.module').then((m) => m.SignUpModule),
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () =>
+      import('./pages/forgot-password/forgot-password.module').then(
+        (m) => m.ForgotPasswordModule,
+      ),
+  },
+  {
+    path: 'reset-password',
+    loadChildren: () =>
+      import('./pages/reset-password/reset-password.module').then(
+        (m) => m.ResetPasswordModule,
+      ),
+  },
+  {
+    path: 'student',
+    canActivate: [authGuard],
+    loadChildren: () =>
+      import('./pages/student/student.module').then((m) => m.StudentModule),
+  },
+  {
+    path: 'payment',
+    // canActivate: [authGuard],
+    loadChildren: () =>
+      import('./pages/payment/payment.module').then((m) => m.PaymentModule),
+  },
 ];
